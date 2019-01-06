7cdac0661e3122ab9d7f34c0cb1514e17512386e
Otavio Araujo <caotavio@hotmail.com>
2019-01-06 17:03:23 -0300

    scripts.js functional but not with proper styles on buttons

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// As there is only one unordered list we can
+// access it like we would in an array with the [0]
+const studentsUL = document.getElementsByTagName('ul')[0];
+const students = document.getElementsByClassName('student-item cf');
+const parentDiv = document.getElementsByClassName('page')[0];
+
+/*
+1.Calculates the indexes of the first and last elements
+based on a max of 10 elements per page.
+2. loops through the list and and shows only the elements
+that fit the criteria given the page number
+*/
+const showPage = (students, page) => {
+  const firstIndex = (page - 1) * 10;
+  const lastIndex = firstIndex + 10 - 1;
+  for (let i = 0; i < students.length; i++) {
+    if (i >= firstIndex && i <=lastIndex) {
+      students[i].style.display = 'block';
+    } else {
+        students[i].style.display = 'none';
+    }
+  }
+}
+
+const appendPageLinks = (parentDiv) => {
+  let pagesTotal = Math.ceil(students.length / 10);
+  let div = document.createElement('div');
+  div.className = 'pagination';
+  let ul = document.createElement('ul');
+  for (let i = 1; i <= pagesTotal; i++) {
+    let li = document.createElement('li');
+    li.className = 'pagination';
+    let a = document.createElement('a');
+    a.className = 'pagination';
+    li.appendChild(a);
+    ul.appendChild(li);
+    if (i === 1) {
+      a.className = 'active';
+      showPage(students, 1);
+    }
+    a.textContent = i;
+    a.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.target.className = 'active';
+      if (a !== e.target) {
+        a.className = '';
+      }
+      showPage(students, i);
+    });
+  }
+  parentDiv.appendChild(ul);
+}
+
+appendPageLinks(parentDiv);
