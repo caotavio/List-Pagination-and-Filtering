0a396ca0fc918a7c0150db3a60703f0a413e1a49
Otavio Araujo <caotavio@hotmail.com>
2019-01-07 21:57:20 -0300

    scripts.js working and styled

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-// As there is only one unordered list we can
-// access it like we would in an array with the [0]
+const parentDiv = document.getElementsByClassName('page')[0];
 const studentsUL = document.getElementsByTagName('ul')[0];
 const students = document.getElementsByClassName('student-item cf');
-const parentDiv = document.getElementsByClassName('page')[0];
 const itemsPerPage = 10;
 
 /*
 1.Calculates the indexes of the first and last elements
-based on a max of 10 elements per page.
+based on a max of elements per page.
 2. loops through the list and and shows only the elements
-that fit the criteria given the page number.
+that fit the criteria given the page number passed as the
+second parameter.
 */
 const showPage = (students, page) => {
   const firstIndex = (page - 1) * itemsPerPage;
@@ -23,6 +22,10 @@ const showPage = (students, page) => {
   }
 }
 
+/*
+Creates links to each page according to the length of the student list and
+appends them to the page dinamically.
+*/
 const appendPageLinks = (parentDiv) => {
   let pagesTotal = Math.ceil(students.length / itemsPerPage);
   let div = document.createElement('div');
@@ -30,16 +33,12 @@ const appendPageLinks = (parentDiv) => {
   parentDiv.appendChild(div);
   let ul = document.createElement('ul');
   div.appendChild(ul);
-  /*
-  This for loop keeps the function dinamic, creating the links based on the
-  number os students and setting a maximum of 10 students per page.
-  */
   for (let i = 1; i <= pagesTotal; i++) {
     let li = document.createElement('li');
     let a = document.createElement('a');
     ul.appendChild(li);
     li.appendChild(a);
-    // Gives the active class for the first page when the page opens.
+    // Gives the first page the active class for css style.
     if (i === 1) {
       a.className = 'active';
       showPage(students, 1);
@@ -53,7 +52,7 @@ const appendPageLinks = (parentDiv) => {
       for (let i = 0; i < links.length; i++) {
         links[i].classList.remove('active');
       }
-      // Gives the active class back just for the link.
+      // Gives the active class back just for the link clicked.
       e.target.className = 'active';
     });
   }
